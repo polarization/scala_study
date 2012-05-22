da88a18db6bcf070497fec7f1f4de178b7947e1a
Zhang Lei (Sneeze) <sneezezhang@cienet.com.cn>
2012-05-22 17:57:42 +0800

    scala java

---
+++ src/com/scala/study/scalajava/JavaApp.java
@@ -0,0 +1,15 @@
+package com.scala.study.scalajava;
+
+public class JavaApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ScalaApp$.MODULE$.says();
+		ScalaApp app = new ScalaApp();
+		app.says();
+	}
+
+}
---
+++ src/com/scala/study/scalajava/ScalaApp.scala
@@ -0,0 +1,13 @@
+package com.scala.study.scalajava
+
+object ScalaApp {
+  def says = {
+    println("Scala App Object Says")
+  }
+}
+
+class ScalaApp {
+  def says = {
+    println("Scala App Class Says")
+  }
+}
